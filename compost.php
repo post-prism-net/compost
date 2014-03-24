25ef36d495b46e71c0af09ecf9a58e2b09fc7e4c
HATSUMATSU <mail@hatsumatsu.de>
2014-03-24 09:42:52 +0100

    remove unneccessary comments

--- compost.php
+++ compost.php
@@ -183,24 +183,20 @@ class compost {
 
 	/** 
 	  * Upload image file 
-	  * > http://php.net/manual/de/features.file-upload.php
+	  * > http://php.net/manual/de/features.file-upload.php#114004
 	  *
 	  * @param 	int 	$id image ID
 	  */
 	static function createImage( $id ) {
 		global $messages;
 
-		
 		try {
 		   
-		    // Undefined | Multiple Files | $_FILES Corruption Attack
-		    // If this request falls under any of them, treat it invalid.
 		    if( !isset( $_FILES['file']['error'] ) 
 		    	|| is_array( $_FILES['file']['error'] ) ) {
 		        throw new RuntimeException( 'Invalid parameters.' );
 		    }
 
-		    // Check $_FILES['upfile']['error'] value.
 		    switch( $_FILES['file']['error'] ) {
 		        case UPLOAD_ERR_OK:
 		            break;
@@ -213,30 +209,23 @@ class compost {
 		            throw new RuntimeException( 'Unknown errors.' );
 		    }
 
-		    // You should also check filesize here.
 		    if( $_FILES['file']['size'] > 20971520 ) {
 		        throw new RuntimeException( 'Exceeded filesize limit.' );
 		        $messages[] = 'File size too big.';
 		    }
 
-		    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
-		    // Check MIME Type by yourself.
 		    $finfo = new finfo( FILEINFO_MIME_TYPE );
 		    if( false === $ext = array_search(
 		        $finfo->file( $_FILES['file']['tmp_name'] ),
 		        array(
 		            'jpg' => 'image/jpeg',
 		            'png' => 'image/png'
-		            /*'gif' => 'image/gif',*/
 		        ),
 		        true
 		    ) ) {
 		        throw new RuntimeException( 'Invalid file format.' );
 		    }
 
-		    // You should name it uniquely.
-		    // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
-		    // On this example, obtain safe unique name from its binary data.
 		    if( !move_uploaded_file(
 		        $_FILES['file']['tmp_name'],
 		        sprintf( '%s/%s.%s',
